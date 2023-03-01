2ef1deb1b3949316a491125b6abc257a6bf97f75
Savina Dasha <64271239+Fawentus@users.noreply.github.com>
2023-03-01 17:42:53 +0300

    Add unknown commands

--- parser_tests.py
+++ parser_tests.py
@@ -1,232 +1,140 @@
-from parser_utils import Parser
-from parser_utils import AbstractCommand # TODO
-from lexer_utils import Lexer
+#from interpreter_utils import AbstractCommand TODO
 import exceptions_utils
+from token_utils import Token_types
 
-def compare(res1, var1, res2, var2):
-    if len(res1) != len(res2) or len(var1) != len(var2):
-        return False
-    for i in range(len(res1)):
-        if res1[i].name != res2[i].name:
-            return False
-        if len(res1[i].args) != len(res2[i].args):
-            return False
-        for j in range(len(res1[i].args)):
-            if res1[i].args[j] != res2[i].args[j]:
-                return False
-    for i in var1:
-        if not i in var2:
-            return False
-        if var1[i] != var2[i]:
-            return False
-    return True
-
-commands = ['echo', 'cat', 'wc', 'pwd', 'exit'] # там на деле dict должен быть
-
-lexer = Lexer()
-parser = Parser()
-
-print("\nTEST1")
-environment_variables = {}
-tokens = lexer.lexer('nc -l 12345 | nc www.google.com 80 | nc www.google.com 80')
-try:
-    parser.parse(tokens, commands, environment_variables)
-except exceptions_utils.InvalidCommand:
-    print("Pass")
-
-print("\nTEST2")
-environment_variables = {}
-right_res = [AbstractCommand('echo', ['1'])]
-right_var = []
-tokens = lexer.lexer('echo 1')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST3")
-environment_variables = {}
-right_res = [AbstractCommand('echo', [])]
-right_var = []
-tokens = lexer.lexer('echo')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST4")
-environment_variables = {}
-tokens = lexer.lexer('echo $var')
-try:
-    res, var = parser.parse(tokens, commands, environment_variables)
-except exceptions_utils.UnknownVariable:
-    print("Pass")
-
-print("\nTEST5")
-environment_variables = {}
-right_res = [AbstractCommand('echo', ['$var'])]
-right_var = []
-tokens = lexer.lexer("echo '$var'")
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST6")
-environment_variables = {}
-right_res = [AbstractCommand('exit', [])]
-right_var = []
-tokens = lexer.lexer("exit")
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST7")
-environment_variables = {}
-tokens = lexer.lexer('echo 23 434 "454" "$dff"')
-try:
-    res, var = parser.parse(tokens, commands, environment_variables)
-except exceptions_utils.UnknownVariable:
-    print("Pass")
-
-print("\nTEST8")
-environment_variables = {}
-right_res = [AbstractCommand('pwd', [])]
-right_var = []
-tokens = lexer.lexer('pwd')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST9")
-environment_variables = {}
-right_res = [AbstractCommand('cat', ['/home/sergiy/tmp/file1'])]
-right_var = []
-tokens = lexer.lexer('cat /home/sergiy/tmp/file1')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST10")
-environment_variables = {}
-right_res = [AbstractCommand('cat', ['C:\\Users\\daled\\Downloads\\file\\baseline'])]
-right_var = []
-tokens = lexer.lexer('cat C:\\Users\\daled\\Downloads\\file\\baseline')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST11")
-environment_variables = {}
-right_res = [AbstractCommand('wc', ['/home/sergiy/tmp/file1'])]
-right_var = []
-tokens = lexer.lexer('wc /home/sergiy/tmp/file1')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST12")
-environment_variables = {}
-right_res = [AbstractCommand('wc', ['C:\\Users\\daled\\Downloads\\file\\baseline'])]
-right_var = []
-tokens = lexer.lexer('wc C:\\Users\\daled\\Downloads\\file\\baseline')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST13")
-environment_variables = {}
-right_res = []
-right_var = {'var': '3'}
-tokens = lexer.lexer('var=3')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST14")
-environment_variables = {}
-tokens = lexer.lexer("var=$var1")
-try:
-    res, var = parser.parse(tokens, commands, environment_variables)
-except exceptions_utils.UnknownVariable:
-    print("Pass")
-
-print("\nTEST15")
-environment_variables = {}
-right_res = []
-right_var = {'var': '$var1'}
-tokens = lexer.lexer("var='$var1'")
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST16")
-environment_variables = {}
-tokens = lexer.lexer('var="$var1"')
-try:
-    res, var = parser.parse(tokens, commands, environment_variables)
-except exceptions_utils.UnknownVariable:
-    print("Pass")
-
-print("\nTEST17")
-environment_variables = {}
-tokens = lexer.lexer('ls -l | grep "\.txt$"')
-try:
-    parser.parse(tokens, commands, environment_variables)
-except exceptions_utils.InvalidCommand:
-    print("Pass")
-
-print("\nTEST18")
-environment_variables = {}
-right_res = [AbstractCommand('echo', ['1'])]
-right_var = {}
-tokens = lexer.lexer('echo 1 | 45 67')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
-
-print("\nTEST19")
-environment_variables = {}
-right_res = []
-right_var = {'var': '5'}
-tokens = lexer.lexer('var=5 echo 1')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
+# TODO надо удалить, когда реализован будет
+class AbstractCommand():
+    def __init__(self, name, args):
+        self.name = name
+        self.args = args
     
-print("\nTEST20")
-environment_variables = {}
-right_res = [AbstractCommand('echo', ['1'])]
-right_var = {'var': '1'}
-tokens = lexer.lexer('var=1')
-res, var = parser.parse(tokens, commands, environment_variables)
-tokens = lexer.lexer('echo $var')
-res, var = parser.parse(tokens, commands, var)
-assert compare(right_res, right_var, res, var)
-print("Pass")
+'Класс парсера'
+class Parser():
+    def __init__(self):
+        self.environment_variables = []
+        self.res = []
 
-print("\nTEST21")
-environment_variables = {}
-right_res = [AbstractCommand('echo', ['123']), AbstractCommand('wc', [])]
-right_var = {}
-tokens = lexer.lexer('echo 123 | wc')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
+    '''Command : Environments_init
+                    | Command_pipeline
+                    | Single_command'''
+    def p_command(self, input):
+        if len(input) == 0:
+            return True
+        if self.p_environments_init(input):
+            return True
+        if self.p_command_pipeline(input):
+            return True
+        if self.p_single_command(input):
+            return True
+        return False
+    
+    '''Environments_init : Environment_init
+                            |  Environment_init Environments_init'''
+    def p_environments_init(self, input):
+        if len(input) == 0:
+            return True
+        if self.p_environment_init(input):
+            return True
+        
+        for i in range(len(input)):
+            if input[i].token_type == Token_types.value:
+                if self.p_environment_init(input[:i+1]) and self.p_environments_init(input[i+1:]):
+                    return True
+                else:
+                    return False
+        return False
 
-print("\nTEST22")
-environment_variables = {}
-right_res = [AbstractCommand('echo', ['1', '1', '1', '1', '1'])]
-right_var = {}
-tokens = lexer.lexer('echo 1 1 1 1 1')
-res, var = parser.parse(tokens, commands, environment_variables)
-assert compare(right_res, right_var, res, var)
-print("Pass")
+    '''Environment_init : VAR ASSIGNMENT VALUE
+                            | VAR ASSIGNMENT VAR'''
+    def p_environment_init(self, input):
+        if len(input) != 3:
+            return False
+        if input[0].token_type == Token_types.var and input[1].token_type == Token_types.assignment and input[2].token_type == Token_types.value:
+            self.environment_variables[input[0].token_value] = input[2].token_value
+            return True
+        if input[0].token_type == Token_types.var and input[1].token_type == Token_types.assignment and input[2].token_type == Token_types.var:
+            if not input[2].token_value in self.environment_variables:
+                raise exceptions_utils.UnknownVariable()
+            self.environment_variables[input[0].token_value] = self.environment_variables[input[2].token_value]
+            return True
+        return False
 
-print("\nTEST23")
-environment_variables = {}
-right_res = [AbstractCommand('echo', ['2', '1', '1', '1', '1', '2'])]
-right_var = {'var': '2'}
-tokens = lexer.lexer('var=2')
-res, var = parser.parse(tokens, commands, environment_variables)
-tokens = lexer.lexer('echo $var 1 1 1 1 $var')
-res, var = parser.parse(tokens, commands, var)
-assert compare(right_res, right_var, res, var)
-print("Pass")
+    '''Command_pipeline : Single_command PIPELINE Command_pipeline
+                            | Single_command'''
+    def p_command_pipeline(self, input):
+        if len(input) == 0:
+            return True
+        for i in range(len(input)):
+            if input[i].token_type == Token_types.pipeline:
+                if self.p_single_command(input[:i]) and self.p_command_pipeline(input[i+1:]):
+                    return True
+                else:
+                    return False
+        if self.p_single_command(input):
+            return True
+        return False
+        
+    'Single_command : COMMAND Args'
+    def p_single_command(self, input):
+        if len(input) == 0 or input[0].token_type != Token_types.command:
+            return False
+        
+        # if not input[0].token_value in self.commands:
+        #     raise exceptions_utils.InvalidCommand() # Мы научились работать с неизвестными командами
+        
+        (fl, args) = self.p_args(input[1:])
+        if not fl:
+            return False
+        
+        self.res += [AbstractCommand(input[0].token_value, args)]
+        return True
+
+    '''Args : ARG
+                | ARG Args
+                | Environment Args
+                | Environment'''
+    def p_args(self, input):
+        if len(input) == 0:
+            return (True, [])
+
+        if len(input) == 1 and input[0].token_type == Token_types.arg:
+            return (True, [input[0].token_value])
+
+        if input[0].token_type == Token_types.arg:
+            (fl, args) = self.p_args(input[1:])
+            if fl:
+                return (True, [input[0].token_value] + args)
+            return (False, [])
+
+        for i in range(len(input)):
+            if input[i].token_type == Token_types.var:
+                (fl, arg) = self.p_environment(input[:i+1])
+                if not fl:
+                    return (False, [])
+                (fl, args) = self.p_args(input[i+1:])
+                if fl:
+                    return (True, [arg] + args)
+                else:
+                    return (False, [])
+        
+        (fl, arg) = self.p_environment(input)
+        if fl:
+            return (True, [arg])
+        
+        return (False, [])
+                
+    'Environment : VAR'
+    def p_environment(self, input):
+        if len(input) != 1 or input[0].token_type != Token_types.var:
+            return (False, None)
+        if not input[0].token_value in self.environment_variables:
+            raise exceptions_utils.UnknownVariable()
+        return (True, self.environment_variables[input[0].token_value])
+
+    'Основная функция, которую нужно вызывать для парсинга'
+    def parse(self, input, environment_variables):
+        self.environment_variables = environment_variables
+        self.res = []
+        if not self.p_command(input):
+            raise exceptions_utils.ParsingError()
+        return (self.res, self.environment_variables)
