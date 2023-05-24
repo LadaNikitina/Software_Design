01a239f046017a8d91758846b47c69412d7313b2
unknown <da.led@mail.ru>
2023-05-24 21:02:05 +0300

    Add character module

--- enemy.py
+++ enemy.py
@@ -7,24 +7,56 @@ class Enemy:
         self.default_strategy = default_strategy
         self.curr_strategy = default_strategy
         self.health = health
+        self.max_health = health
         self.damage = damage
+        self.is_alive = True
 
-    def change_health(self, d_health):
-        self.health += d_health
+    def change_health(self):
+        if self.health <= 0:
+            self.is_alive = False
+            # тут по флагу с карты эта дичь удаляется
+        elif self.health < 0.2 * self.max_health:
+            self.curr_strategy = "defensive"
+        elif self.health >= 0.8 * self.max_health:
+            self.curr_strategy = self.default_strategy
 
-        # Тут еще нужно что-то делать, если враг умер
-        # А еще менять стратегию, если здоровья слишком мало или если оно нормализовалось (мб не порог устанавливать, а брать процент от максимального здоровья?)
-        raise NotImplementedError()
+    def outer_damage(self, damage):
+        self.health = min(0, self.health - damage)
+        self.change_health()
+
+    def hill(self, hill_value):
+        self.health = max(self.max_health, self.health + hill_value)
+        self.change_health()
+
 
 # Функции ниже должны возвращать врага с нужными характеристиками
 # К ним еще символы нужно придумать, берите те, что по ширине как буква латинская, а то бывают символы шире, из-за них карта поедет
 def make_mummy(coordX, coordY):
-    raise NotImplementedError()
+    symbol = "M"
+    experience = 50
+    default_strategy = "aggressive"
+    health = 100
+    damage = 10
+
+    enemy = Enemy(symbol, coordX, coordY, experience, default_strategy, health, damage)
+    return enemy
 
 
 def make_grasshopper(coordX, coordY):
-    raise NotImplementedError()
+    symbol = "G"
+    experience = 30
+    default_strategy = "defensive"
+    health = 50
+    damage = 5
+    enemy = Enemy(symbol, coordX, coordY, experience, default_strategy, health, damage)
+    return enemy
 
 
 def make_lost_traveler(coordX, coordY):
-    raise NotImplementedError()
+    symbol = "T"
+    experience = 20
+    default_strategy = "passive"
+    health = 30
+    damage = 2
+    enemy = Enemy(symbol, coordX, coordY, experience, default_strategy, health, damage)
+    return enemy
--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from map import Map
+from character import Character
+import keyboard
+# написать функцию рандомной генерации позиции по карте
 
 m = Map()
 m.generateMap()
+
+# Создание объекта игрока
+player = Character(0, 0)
+
+# Обработчик нажатия клавиш
+def handle_keypress(event):
+    if event.name == 'up':
+        player.move_up()
+    elif event.name == 'down':
+        player.move_down()
+    elif event.name == 'left':
+        player.move_left()
+    elif event.name == 'right':
+        player.move_right()
+
+# Регистрация обработчика для нажатий клавиш
+keyboard.on_press(handle_keypress)
+
+# Программа будет работать до тех пор, пока не будет нажата клавиша Esc
+keyboard.wait('esc')
+
+# Вывод координат игрока после завершения программы
+print(f"Player coordinates: ({player.coordX}, {player.coordY})")
+
 m.drawMap()
 print('\n')
 m.drawPieceOfMap(centre_x=30, centre_y=24, height=20, width=20)
