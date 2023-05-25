3e4c1229062804833f7cf72943cffc14727dad13
unknown <da.led@mail.ru>
2023-05-25 21:28:06 +0300

    Add moving characters

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from map import Map
 from character import Character
 import keyboard
+from os import system, name
+
 # написать функцию рандомной генерации позиции по карте
 
 m = Map()
 m.generateMap()
 
 # Создание объекта игрока
-player = Character(0, 0)
+player = Character(m.height // 2, m.width // 2,)
+m.drawPieceOfMap(centre_x=m.height // 2, centre_y=m.width // 2, height=20, width=20)
+
 
 # Обработчик нажатия клавиш
 def handle_keypress(event):
     if event.name == 'up':
+        _ = system('cls')
         player.move_up()
+        m.drawPieceOfMap(centre_x=player.coordX, centre_y=player.coordY, height=20, width=20)
     elif event.name == 'down':
+        _ = system('cls')
         player.move_down()
+        m.drawPieceOfMap(centre_x=player.coordX, centre_y=player.coordY, height=20, width=20)
     elif event.name == 'left':
+        _ = system('cls')
         player.move_left()
+        m.drawPieceOfMap(centre_x=player.coordX, centre_y=player.coordY, height=20, width=20)
     elif event.name == 'right':
+        _ = system('cls')
         player.move_right()
+        m.drawPieceOfMap(centre_x=player.coordX, centre_y=player.coordY, height=20, width=20)
+
+
 
 # Регистрация обработчика для нажатий клавиш
 keyboard.on_press(handle_keypress)
@@ -28,13 +42,7 @@ keyboard.wait('esc')
 
 # Вывод координат игрока после завершения программы
 print(f"Player coordinates: ({player.coordX}, {player.coordY})")
-
-m.drawMap()
-print('\n')
-m.drawPieceOfMap(centre_x=30, centre_y=24, height=20, width=20)
-
-m = Map(mode=1)
-m.generateMap()
-m.drawMap()
-print('\n')
-m.drawPieceOfMap(centre_x=30, centre_y=24, height=20, width=20)
+#
+# m.drawMap()
+# print('\n')
+# m.drawPieceOfMap(centre_x=30, centre_y=24, height=20, width=20)
